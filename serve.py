29c95cc49ae929608edfbd194153107c1eb70be2
Yuanwei XU <yuanwxu@126.com>
2019-04-18 14:18:54 +0100

    Update serve.py
    https://www.kaggle.com/yuanwxu/careercon-making-an-app-from-your-modeling-code/output

--- serve.py
+++ serve.py
@@ -1,5 +1,20 @@
-def useless_function():
-    """ 
-    A function that returns some text
-    """
-    return("some text")
+from flashtext.keyword import KeywordProcessor
+import pickle
+
+# Function that takes loads in our pickled word processor
+# and defines a function for using it. This makes it easy
+# to do these steps together when serving our model.
+def get_keywords_api():
+    
+    # read in pickled word processor. You could also load in
+    # other models as this step.
+    keyword_processor = pickle.load(open("processor.pkl", "rb"))
+    
+    # Function to apply our model & extract keywords from a 
+    # provided bit of text
+    def keywords_api(keywordProcessor, text, span_info=True): 
+        keywords_found = keywordProcessor.extract_keywords(text, span_info=True)      
+        return keywords_found
+    
+    # return the function we just defined
+    return keywords_api
