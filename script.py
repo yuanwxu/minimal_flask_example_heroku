4fe815327e3ef768df189bd23a3de7eeff3772e8
Yuanwei XU <yuanwxu@126.com>
2019-04-18 14:33:36 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,30 @@
+import json
 from flask import Flask
-from serve import useless_function  
+from serve import get_keywords_api
 
 app=Flask(__name__)
 
-# Define our "ping" end point
-@app.route('/ping')
-def useless_output():
-  return(useless_function())
+keywords_api = get_keywords_api()
+
+
+# Define a post method for our API.
+@app.route('/extractpackages', methods=['POST'])
+def extractpackages():
+    """ 
+    Takes in a json file, extracts the keywords &
+    their indices and then returns them as a json file.
+    """
+    # the data the user input, in json format
+    input_data = request.json
+
+    # use our API function to get the keywords
+    output_data = keywords_api(input_data)
+
+    # convert our dictionary into a .json file
+    # (returning a dictionary wouldn't be very
+    # helpful for someone querying our API from
+    # java; JSON is more flexible/portable)
+    response = json.dumps(output_data)
+
+    # return our json file
+    return response
