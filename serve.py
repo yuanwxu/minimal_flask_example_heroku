00951336dcf8e4222c291d37fd7423335f3b75f4
Yuanwei XU <yuanwxu@126.com>
2019-04-19 11:37:15 +0100

    Update serve.py

--- serve.py
+++ serve.py
@@ -14,7 +14,7 @@ def get_keywords_api():
     # Function to apply our model & extract keywords from a 
     # provided bit of text
     def keywords_api(text): 
-        keywords_found = keywordProcessor.extract_keywords(text, span_info=True)      
+        keywords_found = keyword_processor.extract_keywords(text, span_info=True)      
         return keywords_found
     
     # return the function we just defined
